a81417ff2e66a6cc46405ddead764e7ce7cbfc06
VARUN KUMAR <varun91thakur@gmail.com>
2024-12-05 10:26:11 +0530

    [VARUN] added: use case 4

--- usecase.rb
+++ usecase.rb
@@ -1,28 +1,29 @@
-randomnumber= rand(2)   
-c= "ABSENT"
-puts randomnumber
-if randomnumber == 0
-  c= "ABSENT"
-else 
-  c= "PRESENT"
-end
 
-puts c
+randomnumber= rand(2)  
+puts randomnumber
+c = ""
+if randomnumber == 0
+    c = "Absent"
+else 
+  c = "Present"
+end
+puts c 
 
 fullday_hours = 8
 halfday_hours = 4
 salaryperhour = 20
 salary =0
-if c == "PRESENT"
-  time= rand(2)
-  if time == 0
-    
-    salary =salaryperhour * halfday_hours
-    puts "HALF DAY"
-  elsif 
-    salary=salaryperhour * fullday_hours
-    puts "FULL DAY"
-    
+if c == "Present"
+  time = rand(2)
+  case time 
+  when 0
+    salary = salaryperhour * halfday_hours
+    puts "Employee is present for half day and salary #{salary}"
+  when 1
+    salary = salaryperhour * fullday_hours
+    puts "Employee is present for full day and salary #{salary}"
   end
-end
-puts "EMPLOYEES DAILY SALARY IS : #{salary}"
+
+
+end 
+
