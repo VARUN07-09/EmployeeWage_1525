5c61a710a9e9505d473253b925ea85ae771cc13c
VARUN KUMAR <varun91thakur@gmail.com>
2024-12-05 11:12:31 +0530

    [VARUN] added: use case 5

--- usecase.rb
+++ usecase.rb
@@ -1,18 +1,20 @@
 
 randomnumber= rand(2)  
-puts randomnumber
+
 c = ""
 if randomnumber == 0
     c = "Absent"
 else 
   c = "Present"
 end
-puts c 
+puts "DAILY ATTENDANCE IS : #{c}" 
 
 fullday_hours = 8
 halfday_hours = 4
 salaryperhour = 20
+workingdays=20
 salary =0
+totalsalary=0
 if c == "Present"
   time = rand(2)
   case time 
@@ -26,4 +28,25 @@ if c == "Present"
 
 
 end 
+puts "Monthly Attendance is :"
+workingdays.times do |days|
+  attendance =rand(3)
+  case attendance
+  when 0
+    puts "Day #{days + 1} ABSENT"
+  when 1
+    salary = salaryperhour * halfday_hours
+    totalsalary+=salary 
+    puts "Day #{days + 1} Employee is present for half day and salary #{salary}"
+  when 2
+    salary = salaryperhour * fullday_hours
+    totalsalary+=salary
+    puts "Day #{days + 1} Employee is present for full day and salary #{salary}"
+  end
+
+end
+puts "Total salary for one month is #{totalsalary}"
+
+
+
 
